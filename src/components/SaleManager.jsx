ad262fa20c9cfe2428816f517afbbd4b83487a9e
vanvu0211 <vu34304@gmail.com>
2025-07-21 12:15:34 +0700

    update

--- src/components/SaleManager.jsx
+++ src/components/SaleManager.jsx
@@ -345,7 +345,7 @@ function SaleManager() {
           </div>
         ) : (
           <>
-            <div className="sticky top-0 bg-gray-50 z-10 pt-2">
+            <div className="sticky top-0  z-10 pt-1">
               <div className="flex flex-wrap gap-2 mb-4 overflow-x-auto">
                 {categories.map((category) => (
                   <button
